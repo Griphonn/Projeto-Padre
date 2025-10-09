2b8f680c4d351e246606be82f764d49d7a5d9757
Griphonn <griphongelido@gmail.com>
2025-10-09 08:16:22 -0300

    Primeiro commit

---
+++ tkinter.py
@@ -0,0 +1,19 @@
+from tkinter import *
+import customtkinter as ctk
+
+
+# Cores do Projeto - Hexadecimal
+cor1 ="#5E27F5" # azul
+cor2 = "#03000A" # preto
+cor3 = "#FFFFFF" # branco
+cor4 = "#DEF720" # amarelo
+cor5 = "#31496B" # azul acinzentado
+
+janela = Tk()
+
+janela.title("Login")
+janela.geometry('300x350')
+janela.configure(background=cor2)
+janela.resizable(width=FALSE ,heigth=FALSE)
+
+janela.mainloop()
